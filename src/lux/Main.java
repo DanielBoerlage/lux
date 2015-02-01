59691468b1fbe4b0460c541475d9bb3fadceba4f
DanielBoerlage <daniel.brlg@gmail.com>
2015-02-01 05:08:18 -0800

    made running the jar file work from any directory and simplified build file

--- src/lux/Main.java
+++ src/lux/Main.java
@@ -1,15 +1,28 @@
 package lux;
 
+import java.net.URISyntaxException;
+
 import org.lwjgl.Sys;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		initNatives();
 		System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
 	}
 
 	private static void initNatives() {
-		System.setProperty("org.lwjgl.librarypath", "lib/");
+		String libdir = "lib/";
+		try {
+			String jar = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+			libdir = jar.substring(0, jar.lastIndexOf("/")) + "/lib/";
+		} catch(URISyntaxException e) {
+			printerr("Error finding lux.jar");
+		}
+		System.setProperty("org.lwjgl.librarypath", libdir);
+	}
+
+	private static void printerr(Object obj) {
+		System.err.print(obj);
 	}
 }
