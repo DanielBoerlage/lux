eb150e6caa7913426cdbb6b30acc4324e839f7ae
DanielBoerlage <daniel.brlg@gmail.com>
2015-02-01 04:55:01 -0800

    moved native lib loading to java code

--- src/lux/Main.java
+++ src/lux/Main.java
@@ -5,6 +5,11 @@ import org.lwjgl.Sys;
 public class Main {
 
 	public static void main(String[] args) {
+		initNatives();
 		System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+	}
+
+	private static void initNatives() {
+		System.setProperty("org.lwjgl.librarypath", "lib/");
 	}
 }
