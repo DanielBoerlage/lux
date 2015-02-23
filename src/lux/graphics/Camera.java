ceb742d5ca4a7dd92a09818306e7751f06d1dbf0
RowanMcCauley <rowan.mccauley@gmail.com>
2015-02-22 20:50:43 -0800

    added camera class

---
+++ src/lux/graphics/Camera.java
@@ -0,0 +1,35 @@
+package lux.graphics;
+
+public class Camera {
+	public static void left() {
+
+	}
+
+	public static void right() {
+		
+	}
+
+	public static void forward() {
+		
+	}
+
+	public static void backward() {
+		
+	}
+
+	public static void rotR() {
+		
+	}
+
+	public static void rotL() {
+		
+	}
+
+	public static void rotU() {
+		
+	}
+
+	public static void rotDs() {
+		
+	}
+}
